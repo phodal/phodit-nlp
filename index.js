6176e3c265e2deec059ed035a7aad03c021b22e3
Phodal HUANG <h@phodal.com>
2018-07-20 10:18:39 +0800

    [T] thinking in keywords

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var nodejieba = require("nodejieba");
-var result = nodejieba.cut("南京市长江大桥");
-console.log(result);
+let nodejieba = require("nodejieba");
+nodejieba.load();
+
+let topN = 1000;
+let data = require('./output.json');
+let allPosts = '';
+for (let item of data) {
+  allPosts = allPosts + item.content;
+}
+console.log(JSON.stringify(nodejieba.extract(allPosts, topN)));
